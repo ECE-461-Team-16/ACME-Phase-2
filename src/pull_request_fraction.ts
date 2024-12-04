4eec2ec304c3b79b9dbda8ec342ccac6e1a5bc33
phambt <brandon.pham22@gmail.com>
2024-12-04 00:24:44 -0500

    test pullrequestfraction.ts

--- src/pull_request_fraction.ts
+++ src/pull_request_fraction.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import logger from './logger';
 
 // Load environment variables
-dotenv.config({ path: '../.env' });
+dotenv.config();
 const TOKEN = process.env.GITHUB_TOKEN;
 
 // GraphQL API endpoint
